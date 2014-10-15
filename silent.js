b39764e9583ecf3063e4bb1323875f4d45fbb555
Martin0809 <maning0809@gmail.com>
2014-10-15 21:15:31 +0800

    update

--- silent.js
+++ silent.js
@@ -1,3 +1,9 @@
 window.onload = function() {
-
+	var imgs = document.getElementsByTagName("img");
+	var imgHeight = imgs[0].offsetHeight;
+	var clientHeight = document.body.clientHeight;
+	for(var i = 0; i < imgs.length; i++) {
+		imgs[i].style.top = "-" + i * (imgHeight + 50) + "px";
+	}
+	document.getElementsByClassName("content")[0].style.top = (clientHeight - imgHeight) / 2 - 116 + "px";
 };
